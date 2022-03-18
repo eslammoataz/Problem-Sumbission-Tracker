dfdcad97003efab2d5b25a33c88bfcaa85cd4bd4
EslamMoataz <eslammezo2323@gmail.com>
2022-03-18 13:09:14 +0200

    Last Version of the Main
    last version of the main the program is working Perfect

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 import xlsxwriter
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-#---------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------#
 # for google sheet credentials
 scope = ["https://spreadsheets.google.com/feeds",
          'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/spreadsheets',
@@ -15,30 +15,20 @@ client = gspread.authorize(creds)
 sheet  = client.open("Project Submission Tracker").sheet1
 sheet_Id = '1aM8pUZxpP-5yxuSRYaaQRK7H_qxtL_62Va4MGkHr7ps'
 
-# Print data will print sheet's data
-# data = sheet.get_all_records()
-#you can use "Sheet.row_values() ,sheet.col_values() , sheet.cell(1,3).value" to print
-
-#---------------------------------------------------------------------------------------------
-
-#Number of Submissions Need to Print
-
-Number_Of_Submissions = 600
-# Number_Of_Submissions = int(input("Enter a Value: \n"))
-Number_Of_Submissions_string = Number_Of_Submissions.__str__()
-url = "https://codeforces.com/api/user.status?handle=okazakii&from=1&count=" + Number_Of_Submissions_string
-
-response = requests.get(url)
-info = response.json()
-
-
-#reseting all values in sheet
+# reseting all values in the sheet
 for i in range(2,32):
     sheet.update_cell(i ,3,0)
+#---------------------------------------------------------------------------------------------#
 
+#Number of Submissions to go through
+Number_Of_Submissions = '700'
+url = "https://codeforces.com/api/user.status?handle=okazakii&from=1&count=" + Number_Of_Submissions
+response = requests.get(url)
+info = response.json()
+#---------------------------------------------------------------------------------------------#
 
 #looping in the list and counting Number Of Solved Problem each day
-for j in range(Number_Of_Submissions):
+for j in range(int(Number_Of_Submissions)):
 
     # Getting Seconds from API
     second = info['result'][j]['creationTimeSeconds']
@@ -56,9 +46,6 @@ for j in range(Number_Of_Submissions):
         prevValue =int(sheet.cell(Day+1 ,3).value)
         sheet.update_cell(Day+1 ,3 , prevValue+1)
         PrevDate = Date
-
-
-
 
 
 # seconds = (datetime.datetime(2020,9,1,23,59,59) - datetime.datetime(1970,1,1)).total_seconds()
