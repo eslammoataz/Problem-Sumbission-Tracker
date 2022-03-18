a419e6d295481c117250b2725eb3406077aae20a
EslamMoataz <eslammezo2323@gmail.com>
2022-03-18 19:54:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,47 +5,78 @@ import time
 import xlsxwriter
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-#---------------------------------------------------------------------------------------------#
+
+# ---------------------------------------------------------------------------------------------#
 # for google sheet credentials
 scope = ["https://spreadsheets.google.com/feeds",
          'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/spreadsheets',
          'https://www.googleapis.com/auth/drive']
-creds =  ServiceAccountCredentials.from_json_keyfile_name("creds.json" ,scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 client = gspread.authorize(creds)
-sheet  = client.open("Project Submission Tracker").sheet1
-sheet_Id = '1aM8pUZxpP-5yxuSRYaaQRK7H_qxtL_62Va4MGkHr7ps'
+sheet = client.open("Project Submission Tracker").sheet1
 
-# reseting all values in the sheet
-for i in range(2,32):
-    sheet.update_cell(i ,3,0)
-#---------------------------------------------------------------------------------------------#
+# Number of Submissions to go through
+Number_Of_Submissions = '500'
 
-#Number of Submissions to go through
-Number_Of_Submissions = '700'
-url = "https://codeforces.com/api/user.status?handle=okazakii&from=1&count=" + Number_Of_Submissions
-response = requests.get(url)
-info = response.json()
-#---------------------------------------------------------------------------------------------#
+# #----------------------------------------------------------------------------------------------#
 
-#looping in the list and counting Number Of Solved Problem each day
+# ESLAM
+#
+# First_Api = "https://codeforces.com/api/user.status?handle=okazakii&from=1&count=" + Number_Of_Submissions
+# response = requests.get(First_Api)
+# info = response.json()
+#
+# map_for_problems = {}
+# for j in range(int(Number_Of_Submissions)):
+#
+#
+#     second = info['result'][j]['creationTimeSeconds']
+#     Date = time.strftime('%m/%d/%y', time.gmtime(second))
+#     Day = int(Date[3] + Date[4])
+#     month = int(Date[1])
+#     ProblemStatus = info['result'][j]['verdict']
+#     ProblemName = info['result'][j]['problem']['name']
+#
+#     if(month==2):
+#         break
+#     if (ProblemName in map_for_problems):
+#         continue
+#
+#     # updating the desired cell of a certain day for User 1
+#     if (ProblemStatus == 'OK' and month==3):
+#         prevValue = int(sheet.cell(Day + 2, 3).value)
+#         sheet.update_cell(Day + 2, 3, prevValue + 1)
+#         map_for_problems[ProblemName]= 1
+
+# #----------------------------------------------------------------------------------------------#
+
+# AMIR
+Second_Api = "https://codeforces.com/api/user.status?handle=itadorii&from=1&count=" + Number_Of_Submissions
+response2 = requests.get(Second_Api)
+info2 = response2.json()
+
+# map to prevent multiple accepted submissions to be counted
+map_for_problems2 = {}
+
 for j in range(int(Number_Of_Submissions)):
 
-    # Getting Seconds from API
-    second = info['result'][j]['creationTimeSeconds']
+    second2 = info2['result'][j]['creationTimeSeconds']
+    Date2 = time.strftime('%m/%d/%y', time.gmtime(second2))
+    Day2 = int(Date2[3] + Date2[4])
+    month2 = int(Date2[1])
+    ProblemStatus2 = info2['result'][j]['verdict']
+    ProblemName2 = info2['result'][j]['problem']['name']
+    if(month2==2):
+        break
 
-    # Converting Seconds into Date
-    Date = time.strftime('%m/%d/%y', time.gmtime(second))
+    # updating the desired cell of a certain day for User 1
+    if(ProblemName2 in map_for_problems2):
+        continue
+    if (ProblemStatus2 == 'OK' and month2 == 3 ):
+        prevValue = int(sheet.cell(Day2 + 2, 5).value)
+        sheet.update_cell(Day2 + 2, 5, prevValue + 1)
+        map_for_problems2[ProblemName2] = 1
 
-    #Variables to check the correct Day and Month
-    Day = int(Date[3]+Date[4])
-    month = int(Date[1])
-    ProblemStatus = info['result'][j]['verdict']
-
-    #updating the desired cell of a certain day
-    if (ProblemStatus == 'OK' and month==3):
-        prevValue =int(sheet.cell(Day+1 ,3).value)
-        sheet.update_cell(Day+1 ,3 , prevValue+1)
-        PrevDate = Date
 
 
 # seconds = (datetime.datetime(2020,9,1,23,59,59) - datetime.datetime(1970,1,1)).total_seconds()
